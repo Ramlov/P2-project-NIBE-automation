a83d60fc67bf2b3f6bc1295083949283522af5d9
Ramlov <mathias.ramlov10@gmail.com>
2023-04-03 16:46:03 +0200

    Rigtig version til RPI

--- RPi/main.py
+++ RPi/main.py
@@ -1,26 +1,11 @@
 import time
+from datetime import datetime
 from network_checker import InternetChecker
 from pulse_detector import PulseDetector
-import os
-import logging
-import datetime
 import threading
+from discordwebhook import Discord
 
-'''Setup stuff for logging'''
-log_dir = "./logs"
-if not os.path.exists(log_dir):
-    os.makedirs(log_dir)
-
-# Get the name of the file from which the log is created
-file_name = os.path.splitext(os.path.basename(__file__))[0]
-
-# Create log file name with current datetime and the file name
-log_filename = file_name + datetime.datetime.now().strftime("_%Y-%m-%d_%H-%M-%S") + ".log"
-log_path = os.path.join(log_dir, log_filename)
-
-logging.basicConfig(filename=log_path, level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
-
-checker = InternetChecker(discord_webhook_url="", ssid="", psk="")
+checker = InternetChecker(ssid="Telenor4443odd", psk="HpQCTN3TL",discord_webhook_url="")
 
 def connect():
     connected = False
@@ -28,25 +13,27 @@ def connect():
     while not connected:
         connected = checker.check_internet_connection()
         if not connected:
-           logging.warning(file_name + 'Failed to connect to network - Retrying')
            time.sleep(10)
 
 
 pd = PulseDetector(db_host="", db_user="", db_password="", db_name="", discord_webhook_url="")
 connect()
 
+def run_pulse_threads():
+    pulse_detection_thread = threading.Thread(target=pd.collect_pulse)
+    pulse_detection_thread.start()
+
+    pulse_submission_thread = threading.Thread(target=pd.submit_pulse_count)
+    pulse_submission_thread.start()
+
+    pulse_detection_thread.join()
+    pulse_submission_thread.join()
+
 while True:
     try:
-        logging.info('Going into pulse detection loop')
-        pulse_detection_thread = threading.Thread(target=pd.collect_pulse)
-        pulse_detection_thread.start()
-
-        pulse_submission_thread = threading.Thread(target=pd.submit_pulse_count)
-        pulse_submission_thread.start()
-
-        # Wait for threads to finish
-        pulse_detection_thread.join()
-        pulse_submission_thread.join()
-    except PulseDetector.DatabaseConnectionError as e:
-        logging.error(f'An error occured - Reconnicting network - Possible errorcode: {e}')
+        run_pulse_threads()
+    except:
+        pulse_detection_thread.cancel()
+        pulse_submission_thread.cancel()
         connect()
+
