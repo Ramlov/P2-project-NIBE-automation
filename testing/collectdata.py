a44b4842460f1de4d28bcc32cbba36514cbb469c
Ramlov <mathias.ramlov10@gmail.com>
2023-04-19 10:29:58 +0200

    script to collect data for testing

---
+++ testing/collectdata.py
@@ -0,0 +1,40 @@
+
+import pymysql
+import json
+from datetime import datetime, timedelta
+
+def usage():
+    with open('config.json', 'r') as file:
+        config = json.load(file)
+    db_host = config['database']['host']
+    db_name = config['database']['dbname']
+    db_username = config['database']['username']
+    db_password = config['database']['password']
+
+    db_connection = pymysql.connect(host=db_host, database=db_name, user=db_username, password=db_password)
+    db_cursor = db_connection.cursor()
+
+    start_time_str = input("Enter a start time (YYYY-MM-DD): ")
+    end_time_str = input("Enter an end time (YYYY-MM-DD): ")
+
+    start_time_str += " 00:00:00"   
+    end_time_str += " 00:00:00"
+
+    start_time = datetime.strptime(start_time_str, '%Y-%m-%d %H:%M:%S')
+    end_time = datetime.strptime(end_time_str, '%Y-%m-%d %H:%M:%S')
+
+    query = f"SELECT value, price FROM pulse_data WHERE time BETWEEN '{start_time}' AND '{end_time}'"
+    db_cursor.execute(query)
+    rows = db_cursor.fetchall()
+    db_cursor.close()
+    db_connection.close()
+
+    sum_value = sum(row[0] for row in rows) / 1000
+    avg_price = sum(row[1] for row in rows) / len(rows)
+    total = avg_price * sum_value
+    return f'The total KwH usage in periode: {sum_value}. \nThe average price in periode: {avg_price} DKK. \nThe total cost in periode: {total} DKK'
+
+
+
+print(usage())
+
