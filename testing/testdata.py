13e3af81912b8ad645f320eb030b1341d95473bd
Ramlov <mathias.ramlov10@gmail.com>
2023-04-26 23:06:43 +0200

    no

--- testing/testdata.py
+++ testing/testdata.py
@@ -50,4 +50,24 @@ def usage():
 
     return output
 
+
+
+    def pushdata():
+        creds_path = 'exalted-iridium-384920-14a0f16a5466.json'
+        doc_id = '1ArcpUMjb5Tu5EK5GQlwZehQ2oUJnGYjcET4OzTKqhGM'
+        worksheet_name = 'Data'
+        csv_path = 'usage.csv'
+        scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+        creds = ServiceAccountCredentials.from_json_keyfile_name(creds_path, scope)
+        client = gspread.authorize(creds)
+
+        doc = client.open_by_key(doc_id)
+        worksheet = doc.worksheet(worksheet_name)
+
+        with open(csv_path, 'r') as f:
+            reader = csv.reader(f)
+            data = list(reader)
+            worksheet.append_rows(data)
+            print('Data inserted successfully.')
 print(usage())
+pushdata()
